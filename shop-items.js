90ea126c8c9628b24fff8c4ec94dfdef230dd208
Brent <33323646+brentttt@users.noreply.github.com>
2021-07-16 08:14:01 -0400

    Update shop-items.js

--- shop-items.js
+++ shop-items.js
@@ -7,22 +7,8 @@ var shopItems = [
     "price": 70,
     "sold": false
   }, {
-    "name": "bowl 1",
-    "images": ["bowl1.jpg"],
-    "size": "4in x 4in x 2in",
-    "materials": "pear, ash, milk paint",
-    "price": 50,
-    "sold": true
-  }, {
-    "name": "bowl 3",
-    "images": ["bowl3.jpg"],
-    "size": "4in x 4in x 2in",
-    "materials": "pear, milk paint",
-    "price": 70,
-    "sold": false
-  }, {
     "name": "bowl 8",
-    "images": ["bowl8.jpg", "bowl3.jpg"],
+    "images": ["bowl8.jpg"],
     "size": "4in x 4in x 2in",
     "materials": "pear, brass, milk paint",
     "price": 80,
